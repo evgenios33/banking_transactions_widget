e3859f28de63442f0d2a7194d1a4917598ffb5f4
Evgenii Osipenko <e.s.osipenko33@gmail.com>
2026-07-03 22:21:20 +0500

    update gitignore

---
+++ main.py
@@ -0,0 +1,5 @@
+from src import masks
+
+
+print(masks.get_mask_card_number(7000792289606361))
+print(masks.get_mask_account(73654108430135874305))
---
+++ src/widget.py
@@ -0,0 +1,6 @@
+def mask_account_card(type_and_num: str) -> str:
+    pass
+
+
+def get_date():
+    pass
