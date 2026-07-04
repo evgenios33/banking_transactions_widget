cdab447beaa267210d3fdde5bb40c055a37ab6e1
Evgenii Osipenko <e.s.osipenko33@gmail.com>
2026-07-04 14:01:29 +0500

    add new import

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from src import masks
+from src.widget import get_date, mask_account_card
 
 
-print(masks.get_mask_card_number(7000792289606361))
-print(masks.get_mask_account(73654108430135874305))
+print(get_date("2024-03-11T02:26:18.671407"))
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
