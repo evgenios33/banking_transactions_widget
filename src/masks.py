26d9f6a086f5ebe2724952a69e4f850c5297e162
Evgenii Osipenko <e.s.osipenko33@gmail.com>
2026-07-19 16:46:37 +0300

    edited the functions in masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,25 @@
-def get_mask_card_number(card_num: int) -> str:
+def get_mask_card_number(card_num: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску в формате 1234 56** **** 7890."""
-    # Преобразуем номер карты в строку.
-    str_card_num = str(card_num)
-    # Создаем новую строку с помощью срезов и конкатенации.
-    card_num_mask = str_card_num[:4] + " " + str_card_num[4:6] + "** **** " + str_card_num[-4:]
-    # Возвращаем новую строку.
+    formatted_card_num = card_num.replace(" ", "")
+
+    if len(formatted_card_num) != 16:
+        raise ValueError("Некорректный ввод. Введите 16-значный номер карты.")
+
+    if not formatted_card_num.isdigit():
+        raise ValueError("Некорректный ввод. Номер карты не может содержать буквы.")
+
+    card_num_mask = formatted_card_num[:4] + " " + formatted_card_num[4:6] + "** **** " + formatted_card_num[-4:]
     return card_num_mask
 
 
-def get_mask_account(account_num: int) -> str:
+def get_mask_account(account_num: str) -> str:
     """Принимает на вход номер счета и возвращает его маску в формате **7890."""
-    # Преобразуем номер счета в строку.
-    str_account_num = str(account_num)
-    # Создаем новую строку с помощью срезов и конкатенации.
-    account_num_mask = "**" + str_account_num[-4:]
-    # Возвращаем новую строку.
+    formatted_account_num = account_num.replace(" ", "")
+    if len(formatted_account_num) != 20:
+        raise ValueError("Некорректный ввод. Введите 20-значный номер счета.")
+
+    if not formatted_account_num.isdigit():
+        raise ValueError("Некорректный ввод. Номер счета не может содержать буквы.")
+
+    account_num_mask = "**" + formatted_account_num[-4:]
     return account_num_mask
