259ab15c2183a6ff9671420823311d41ab7cf5d2
Evgenii Osipenko <e.s.osipenko33@gmail.com>
2026-07-19 16:50:05 +0300

    edited the functions in widget.py

--- src/widget.py
+++ src/widget.py
@@ -12,31 +12,37 @@ def find_cyrillic_letters(type_and_num: str) -> bool:
 def mask_account_card(type_and_num: str) -> str:
     """Обрабатывает информацию о банковских картах и счетах
     и возвращает строку с замаскированным номером в нужном формате."""
-    if type_and_num == "":
-        incorrect_input = """Некорректный ввод! Введите данные в формате:
-1. Для банковских карт - тип продукта на английском языке и 16-значный номер карты
-(Пример: Visa Platinum 7000792289606361).
-2. Для банковского счета - слово "Счет" и 20-значный номер счета
-(Пример: Счет 73654108430135874305)."""
-        return incorrect_input
-    elif find_cyrillic_letters(type_and_num):
+    if len(type_and_num) < 20:
+        raise ValueError("""Некорректный ввод! Введите данные в формате:
+        1. Для банковских карт - тип продукта на английском языке и 16-значный номер карты
+        (Пример: Visa Platinum 7000792289606361).
+        2. Для банковского счета - слово "Счет" и 20-значный номер счета
+        (Пример: Счет 73654108430135874305).""")
+
+    if find_cyrillic_letters(type_and_num):
         list_of_substr = type_and_num.strip().rsplit(" ", 1)
         type_product = "".join(list_of_substr[0])
         account_num = "".join(list_of_substr[-1])
-        mask_account_num = get_mask_account(int(account_num))
+        mask_account_num = get_mask_account(account_num)
         result = type_product + " " + mask_account_num
         return result
+
     else:
         list_of_substr = type_and_num.strip().rsplit(" ", 1)
         type_product = "".join(list_of_substr[0])
         card_num = "".join(list_of_substr[-1])
-        mask_card_num = get_mask_card_number(int(card_num))
+        mask_card_num = get_mask_card_number(card_num)
         result = type_product + " " + mask_card_num
         return result
 
 
 def get_date(date: str) -> str:
     """Возвращает строку с датой в формате "ДД.ММ.ГГГГ"."""
-    iso_date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    formatted_date = date.strip()
+
+    if len(formatted_date) < 21:
+        raise ValueError("Некорректный ввод. Введите дату в формате '%Y-%m-%dT%H:%M:%S.%f'.")
+
+    iso_date = datetime.strptime(formatted_date, "%Y-%m-%dT%H:%M:%S.%f")
     new_date_format = iso_date.strftime("%d.%m.%Y")
     return new_date_format
